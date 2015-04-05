6453c615964e40fc8ca8ff6fad7b0d5a9d23a1d4
wangxb <wangxb@centos.linux>
2015-04-05 18:11:06 +0800

    pdo三种连接数据库方式一：通过参数形式连接数据库

---
+++ pdo/pdo_connect_1.php
@@ -0,0 +1,14 @@
+<?php
+// 通过参数形式连接数据库
+try{
+	$dsn = 'mysql:host=localhost;dbname=pdotest';
+	$name = 'root';
+	$passwd = 'root';
+	$pdo = new PDO($dsn, $name, $passwd);
+	var_dump($pdo);
+}catch(PDOException $e){
+	echo $e->getMessage();
+}
+
+
+

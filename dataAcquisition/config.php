3777cc72ca71585c024b34dac50926005e786928
wangxb <wxb0328@gmail.com>
2015-09-02 17:56:56 +0800

    complete

---
+++ dataAcquisition/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PWD', 'root');
+define('DB_NAME', 'caiji');
+define('DB_PORT', '3306');
+define('DB_TYPE', '');
+define('DB_CHARSET', 'utf8');
+
+
